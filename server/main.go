1ea21902b46a99c10c1d0b2ca63aae0d53637107
ReactiveX22 <akibreactive@gmail.com>
2025-03-26 22:39:35 +0600

    feat: add create todo api

--- server/main.go
+++ server/main.go
@@ -6,11 +6,40 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello World."})
+	})
+
+	app.Post("/api/todos", func(c fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.Bind().Body(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"msg": "Bad Request. Body is Empty"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(fiber.Map{
+			"msg":  "Todo created successfully",
+			"todo": todo,
+		})
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
